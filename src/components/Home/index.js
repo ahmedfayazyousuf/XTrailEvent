f45f4bee83d3755a0fac365f78d3a62ddd3a9b19
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-02-24 10:06:53 +0400

    q
    q

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,92 +1,25 @@
-import firebase from "../../firbase"
-import { useState } from "react";
-
-import { useEffect } from "react";
+import xtrail from '../Z_Styles/xtrail.png'
+import nislogo from '../Z_Styles/nislogo.png'
 
 const  Home = () =>{
-    const [data, setData] = useState([]);
-    function toDataURL(url) {
-        return fetch(url)
-          .then((response) => {
-            return response.blob();
-          })
-          .then((blob) => {
-            return URL.createObjectURL(blob);
-          });
-      }
-
-      async function getData(){
-
-        const res = await fetch(`https://photocloud.azurewebsites.net/photos`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-
-        var data1 = await res.json();
-
-        setData(data1)
-
-        console.log(data1)
-      }
-
-
-
-      useEffect(()=>{
-        getData();
-      },[])
-
-    function handleClick(){
-
-        data.forEach(async (doc,index) => {
-            var data = doc.Link
-
-            const a = document.createElement("a");
-            a.href = await toDataURL(data);
-            a.download = "myImage.png";
-            document.body.appendChild(a);
-            a.click();
-            document.body.removeChild(a);
-
-           
-        })
-
-
-        // const Image = firebase.firestore().collection("Images64").get().then(async (docs)=>{
-        //     docs.forEach(async (doc,index) => {
-        //         var data = doc.data().image;
-
-        //         const a = document.createElement("a");
-        //         a.href = await toDataURL(data);
-        //         a.download = "myImage.png";
-        //         document.body.appendChild(a);
-        //         a.click();
-        //         document.body.removeChild(a);
-
-               
-        //     })
-        // });
-
-        
-        
-    }
-
-
-    
 return(
     <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height:'100vh'}}>
-        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height:'50vh'}}>
-            <h1>Howdy Admin!</h1>
-            <button type="submit" className="grab" style={{width:"220px", marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'black', backgroundColor: 'white', cursor: 'grab'}} onClick={handleClick}>Download All Images</button>
+
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100%', height:'70%'}}>
+
+          <div style={{justifyContent: 'center', alignItems: 'center'}}>
+              <img id='head' style={{width: '150px'}} src={nislogo} alt="Nissan Laptop Cover"/>
+          </div>
+
+          <h1 style={{fontWeight:'1000', fontSize: '60px', marginTop: '15px'}}>WELCOME</h1>
+          <button type="submit" style={{width:"180px", borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab', marginTop: '-20px'}}>ENTER</button>
+
         </div> 
 
-        <div style={{display: 'flex', flexDirection: 'column', width: '100vw', height:'50vh'}}>
-            <p style={{color: 'white', paddingTop: '100px'}}>Photo Mosaic App</p>
-        </div> 
-            
-            
-            
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'flex-end', width: '100%', height:'30%'}}>
+          <img style={{minWidth: '100px', maxWidth: '250px', margin: '20px'}} src={xtrail} alt="tick"/>
+        </div>
+
     </div>
 )
 }
--- src/components/QRScan/index.js
+++ src/components/QRScan/index.js
@@ -5,56 +5,133 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import { useNavigate } from 'react-router-dom';
 import {useRef} from 'react';
 import axios from "axios";
-import {QrReader} from "react-qr-reader";
-import './qrscan.css'
-
 
 const QRScan = () =>{
 
     const [file, setFile] = useState("");
     const [count, setCount] = useState(0);
     const navigate = useNavigate();
-    const buttonRef = useRef(null);
+    const buttonRef = useRef(null); 
 
-    function handleScan  (data)  {      
- 
-    };
-
-
+    function handleChange(event) {
+      setCount(count+event.target.files.length)
+      console.log(event.target.files[0])
       
+      if(count !== 0){
+        document.getElementById("name").innerHTML = `${count+1} images selected`
+      }
+      
+      for (let i = 0; i < event.target.files.length; i++) { 
+        if(event.target.files.length>1){
+          document.getElementById("name").innerHTML = `${count + event.target.files.length} images selected`
+        }
+
+        if(count === 0 && event.target.files.length === 1){
+          document.getElementById("name").innerHTML = document.getElementById("name").innerHTML = event.target.files[i].name
+        }
+        // else if(i===0){
+        //   document.getElementById("name").innerHTML = event.target.files[i].name
+        // }
+
+
+        const newImage = event.target.files[i];
+        newImage["id"] = Math.random();
+        setFile((prevState) => [...prevState, newImage]);
+      }
+
+    }
+
+    const toBase64 = file => new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+  });
+
+  async function Main(file) {
+    try {
+       const result = await toBase64(file);
+       return result
+    } catch(error) {
+       console.error(error);
+       return;
+    }
+    //...
+ }
+
+
+
+    function sendData (){
+
+      buttonRef.current.disabled = true;
+
+      file.map((file,index)=>{
+        var lol = Main(file).then(data =>{
+          console.log(data)
+          const storage = getStorage();
+          var storagePath = 'uploads/' + file.name ;
+          const storageRef = ref(storage, storagePath);
+          const uploadTask = uploadBytesResumable(storageRef, file);
   
-    function handleError (err)  {
-      console.error(err);
-    };
+          uploadTask.on('state_changed', (snapshot) => {
+              // progrss function ....
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+            }, 
+            (error) => { 
+              // error function ....
+              console.log(error);
+            }, 
+            () => {
+              // complete function ....
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                const Image = firebase.firestore().collection("Images");
+                const Image64= firebase.firestore().collection("Images64");
+                Image.add({imageURL:downloadURL});
+                // Image64.add({image:data});
+                axios.post("https://photocloud.azurewebsites.net/upload", {
+                  downloadURL
+                    },{headers: {
+                      'Access-Control-Allow-Origin': '*',
+                      'Content-Type': 'application/json',
+              }})
+                navigate(`/Success`)
+              });
+            });
+        });
+      })
+      
+        
+        
+
+
+ 
+    }
 
 
     return(
-      <div className="qrscan">
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100vw', height:'100vh'}}>
+          <div style={{display: 'flex', flexDirection: 'column', width: '100vw', height:'50%',alignItems:"center"}}>
+              <div style={{padding: '40px', marginTop: '60px', marginBottom:"-30px"}}>
+                <h3>Please upload an image you would like to add to the mosaic.</h3>
+              </div>
+              <input title="" id="image-file" multiple accept="image/*" type="file" onChange={handleChange} style={{color: 'transparent',marginBottom: "2%", width:"230px",display:"",textAlign:"center",justifyContent:"center", alignItems:"center"}} /> 
+              <label id="name">No Image Selected</label>
+ 
+          </div>
 
-        <div style={{position:'absolute', height:'400px', border:'2px solid',zIndex:'1000', width:'500px', background:'white', display:'flex', justifyContent:'center', alignItems:'center'}}>
 
+          <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height:'50%'}}>
+            <button ref={buttonRef} id="signup" name="signup" type="submit" className="grab" style={{width:"230px",height: "50px", marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'black', backgroundColor: 'white', cursor: 'grab'}} onClick={sendData}>UPLOAD</button>
+            
+          <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                    <img src="https://firebasestorage.googleapis.com/v0/b/assetuploadformbuilder.appspot.com/o/images%2Fgetreadywhite.png?alt=media&token=e12d79b3-89f3-4550-971d-b5cd38152986" alt="Logo" style={{width: '20vh', marginTop: '5px', marginBottom: '10px'}}/>
+                </div>
+          </div>
+
+          
         </div>
-
-        <div style={{height:'100vh', width:'100vw', position:'absolute', background:'black', opacity:'0.5', display:'none'}}>
-
-        </div>
-        <div className="scan">
-
-        </div>
-        <div className="scanframe" style={{zIndex:'1'}}>
-        <QrReader
-            constraints = {{facingMode: 'environment'} }
-            scanDelay={5000}
-            //style={previewStyle}
-            onError={handleError}
-            onResult={handleScan}
-            videoStyle={{height:'100%',width:'97%',padding:0,left:4,top:0,alignSelf:'center'}}
-            videoContainerStyle={{height:'100%', width:'100%',left:0,padding:0}}
-            containerStyle={{height:'80%',left:'15%',width:'70%',top:0}}
-
-        />
-        </div>
-    </div>
     )
 }
 
