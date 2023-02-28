9282316c93d9b7a239dd71b1e2afadaa6203f3f9
Adithya Suresh <55681300+AdiS0928@users.noreply.github.com>
2023-02-28 11:58:44 +0400

    Update index.js

--- src/components/ThankYou/index.js
+++ src/components/ThankYou/index.js
@@ -1,8 +1,15 @@
 const ThankYou = () =>{
     return(
-        <div>
-            
-        </div>
+    <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems:'center', width: '100vw', height:'100vh'}}>
+{/*           
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', width: '100vw', height:'50vh'}}>
+            <img src= {Tick} alt="Logo" style={{width: '140px', marginTop: '20px', marginRight: '20px'}}/>
+        </div> */}
+
+        <div style={{ width: '100vw'}}>
+          <h1 style={{color: '#000', fontSize:'100px'}}>Thank You!</h1>
+        </div>     
+      </div>
     )
 }
 
