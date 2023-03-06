9acf9eb088b25001f31e1c5dce5d816719efa5bf
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-03-06 17:30:36 +0400

    i
    i

--- src/components/UploadImage/index.js
+++ src/components/UploadImage/index.js
@@ -2,8 +2,111 @@ import nislogo from '../Z_Styles/Xtrail Expeditiion.png'
 import nislogo2 from '../Z_Styles/Nissan Logo.png'
 import rostawami from '../Z_Styles/AWR Logo.png'
 import photos from '../Z_Styles/Photos.png'
+import { useState } from "react";
+import firebase from "../../firbase"
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { useNavigate } from 'react-router-dom';
+import {useRef} from 'react';
+import axios from "axios";
 
 const  UploadImage = () =>{
+
+    const [file, setFile] = useState("");
+    const [count, setCount] = useState(0);
+    const navigate = useNavigate();
+    const buttonRef = useRef(null);
+
+    function handleChange(event) {
+      setCount(count+event.target.files.length)
+      console.log(event.target.files[0])
+      
+      if(count !== 0){
+        document.getElementById("name").innerHTML = `${count+1} images selected`
+      }
+      
+      for (let i = 0; i < event.target.files.length; i++) { 
+        if(event.target.files.length>1){
+          document.getElementById("name").innerHTML = `${count + event.target.files.length} images selected`
+        }
+
+        if(count === 0 && event.target.files.length === 1){
+          document.getElementById("name").innerHTML = document.getElementById("name").innerHTML = event.target.files[i].name
+        }
+        // else if(i===0){
+        //   document.getElementById("name").innerHTML = event.target.files[i].name
+        // }
+
+
+        const newImage = event.target.files[i];
+        newImage["id"] = Math.random();
+        setFile((prevState) => [...prevState, newImage]);
+      }
+
+    }
+
+    const toBase64 = file => new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+  });
+
+  async function Main(file) {
+    try {
+       const result = await toBase64(file);
+       return result
+    } catch(error) {
+       console.error(error);
+       return;
+    }
+    //...
+ }
+
+
+
+    function sendData (){
+
+      buttonRef.current.disabled = true;
+
+      file.map((file,index)=>{
+        var lol = Main(file).then(data =>{
+          console.log(data)
+          const storage = getStorage();
+          var storagePath = 'uploads/' + file.name ;
+          const storageRef = ref(storage, storagePath);
+          const uploadTask = uploadBytesResumable(storageRef, file);
+  
+          uploadTask.on('state_changed', (snapshot) => {
+              // progrss function ....
+              const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+              console.log('Upload is ' + progress + '% done');
+            }, 
+            (error) => { 
+              // error function ....
+              console.log(error);
+            }, 
+            () => {
+              // complete function ....
+              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                console.log('File available at', downloadURL);
+                const Image = firebase.firestore().collection("Images");
+                const Image64= firebase.firestore().collection("Images64");
+                Image.add({imageURL:downloadURL});
+                // Image64.add({image:data});
+                axios.post("https://photocloud.azurewebsites.net/upload", {
+                  downloadURL
+                    },{headers: {
+                      'Access-Control-Allow-Origin': '*',
+                      'Content-Type': 'application/json',
+              }})
+                navigate(`/Success`)
+              });
+            });
+        });
+      })
+      
+ 
+    }
 
 return(
     <>
@@ -34,12 +137,11 @@ return(
                 </div>
 
                 <p style={{color: 'black', fontSize: '22px', fontWeight: '0', marginBottom: '5px', marginTop: '-15px'}}>Please upload any images taken on your expedition</p>
+                
+                <input title="" id="image-file" multiple accept="image/*" type="file" onChange={handleChange} style={{color: 'transparent', width:"230px",display:"",textAlign:"center",justifyContent:"center", alignItems:"center", marginBottom: '-20px'}} /> 
+                <label id="name">No Image Selected</label>
 
-            
-                <button type="submit" style={{maxWidth: '700px', minWidth: '250px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab', borderRadius: '30px', marginBottom: '10px'}}>Choose Files</button>
-
-                <button type="submit" style={{maxWidth: '700px', minWidth: '250px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab', borderRadius: '30px', marginBottom: '100px'}}>Upload</button>
-
+                <button ref={buttonRef} id="signup" name="signup" type="submit" className="grab" style={{width:"230px",height: "50px", marginBottom: '40px', borderRadius: '10px', padding: '10px', color: 'white', backgroundColor: 'black', cursor: 'grab'}} onClick={sendData}>UPLOAD</button>
         
         </div> 
     </div>
