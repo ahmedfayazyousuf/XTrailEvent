a7eab117b905875fc40ece65840af266298f6d50
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-03-06 14:59:19 +0400

    changes in ui
    changes in ui

--- src/components/Registration/index.js
+++ src/components/Registration/index.js
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
-import nislogo from '../Z_Styles/nislogo.png'
+import nislogo from '../Z_Styles/Xtrail Expeditiion.png'
+import nislogo2 from '../Z_Styles/Nissan Logo.png'
+import rostawami from '../Z_Styles/AWR Logo.png'
 import firebase from '../../firbase';
 import { useNavigate } from 'react-router-dom';
-import Tick from './tickicon.png'
 
 const Registration = () =>{
 
@@ -21,52 +22,59 @@ const Registration = () =>{
       time: firebase.firestore.FieldValue.serverTimestamp()
 
   }).then((doc)=>{
-    
       navigate('/instructions',{state:{id:doc.id}})
   })
 
   }
 
     return(
-      <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "100vh", justifyContent:"center", alignItems:"center"}}>
+      <>
+        <div style={{display: 'flex', flexDirection: 'row', width: '100%', alignItems: 'center', justifyContent:'center'}}>
+          <div style={{display: 'flex', flexDirection: 'column', width:'50%'}}>
+            <img id='head' style={{width: '100px', marginLeft: '10px'}} src={nislogo2} alt="Nissan Laptop Cover"/>
+          </div>
+
+          <div style={{display: 'flex', flexDirection: 'column', width:'50%', textAlign: 'right', justifyContent: 'flex-end', alignItems: 'flex-end'}}>
+            <img id='head' style={{width: '180px', textAlign: 'right', marginRight: '10px'}} src={rostawami} alt="Nissan Laptop Cover"/>
+          </div>
+        </div>
+
+
+      <div style={{display:"flex", flexDirection:"column", width:"100vw", height: "80vh", justifyContent:"center", alignItems:"center"}}>
 
         <div style={{display: 'flex', flexDirection: 'column', width: '70%', gap:'5px', alignItems: 'center', justifyContent:'center'}}>
 
           <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-            <img id='head' style={{width: '150px'}} src={nislogo} alt="Nissan Laptop Cover"/>
+            <img id='head' style={{width: '300px'}} src={nislogo} alt="Nissan Laptop Cover"/>
           </div>
 
-          <h1 style={{paddingRight: '50px', paddingLeft: '50px', color: 'black', fontSize: '25px'}}>REGISTRATION</h1>
+          <p style={{paddingRight: '50px', paddingLeft: '50px', color: 'black', fontSize: '22px', fontWeight: '0'}}>Register</p>
           
           <div style={{width:"100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-              <input type="text" placeholder='NAME' id="Name" style={{background:"transparent", borderRadius: '10px', border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white'}}/> 
+              <input type="text" placeholder='Name' id="Name" style={{background:"transparent", border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white'}}/> 
           </div>
           
           <div style={{width:"100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-              <input type="email" placeholder='EMAIL' id='email' style={{background:"transparent", borderRadius: '10px', border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white' }} />
+              <input type="email" placeholder='Email' id='email' style={{background:"transparent", border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white' }} />
           </div>
 
  
           <div style={{width:"100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                  <input type="number" placeholder='MOBILE (971 xx xxx xxxx)' id='no' style={{background:"transparent", borderRadius: '10px', border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white' }} />
+                  <input type="number" placeholder='Phone (971 xx xxx xxxx)' id='no' style={{background:"transparent", border:"1px solid black", marginBottom:'15px', width:"100%", height:'27px', color:"black", paddingLeft: '10px', paddingRight: '10px', backgroundColor: 'white' }} />
           </div>
-
-          {/* <div style={{width:"100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-              <textarea className="form-control" type="textarea" name="message" id="message" rows='3' placeholder="For us, the answer is Yes. Now, what's your question?" required style={{height: '115px', marginBottom: '0px', width: '100%', border: '1px solid black', borderRadius: '10px', justifyContent: 'center', alignItems: 'center', paddingLeft: '10px', paddintTop: '10px', paddingRight: '10px', backgroundColor: 'white'}}/>
-          </div> */}
 
           <div>
               <p id='error' style={{color:"red", marginBottom: '5px'}}></p>
           </div>
 
-          <div style={{display: 'flex', width: '100%', flexDirection: 'row', justifyContent: 'center'}}>   
- 
-            <button style={{cursor: 'grab', width: '150px', height: '35px', padding: '5px', border: 'none', borderRadius: '10px', marginLeft: '20px', backgroundColor: 'black', color: 'white'}} onClick={Handlesubmit} variant="contained">SUBMIT</button>
-
+          <div style={{width:"100%", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>  
+            <button style={{cursor: 'grab', width:"100%", height:'40px', padding: '5px', border: 'none', backgroundColor: 'black', color: 'white', borderRadius: '30px'}} onClick={Handlesubmit} variant="contained">SUBMIT</button>
           </div>
 
         </div>
       </div>
+
+      </>
 
     )
 }
