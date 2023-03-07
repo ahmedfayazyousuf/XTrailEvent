f7f127b88c007839f652080aa2c0cea539417ce7
ahmedfayazyousuf <95180025+ahmedfayazyousuf@users.noreply.github.com>
2023-03-07 12:43:12 +0400

    Update index.js

--- src/components/QRScan/index.js
+++ src/components/QRScan/index.js
@@ -138,7 +138,7 @@ const QRScan = () =>{
         <div className="scanframe" style={{zIndex:'1'}}>
         <div style={{display:'flex',flexDirection:'column', alignItems:'center'}}>
           <img id='head' style={{width: '100px', marginLeft: '10px', zIndex: '100000000000000'}} src={desterrain} alt="Nissan Laptop Cover"/>
-          <img id='head' style={{width: '60%', marginLeft: '10px', zIndex: '100000000000000'}} src={scan} alt="Nissan Laptop Cover"/>
+          <img id='head' style={{width: '80%', marginLeft: '10px', zIndex: '100000000000000'}} src={scan} alt="Nissan Laptop Cover"/>
         </div>
         <QrReader
             constraints = {{facingMode: 'environment'} }
